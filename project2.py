2af8827c7a150ae316feb6bfed8bcf7fbda61275
H Son <sonbyj01@gmail.com>
2020-02-27 18:29:24 -0800

    updated project 2

--- project2.py
+++ project2.py
@@ -9,12 +9,13 @@ def scan_ports(addrP, portsP = None):
     if portsP == None:
         port_range = ['1', '1024']
     else:
-        port_range = portsP.split(':')
+        port_range = portsP.split(',')
     remoteServerIP = socket.gethostbyname(addrP)  # performs a dns look up
     print("Checking from ports {} to {} on addr {}".format(port_range[0], port_range[1], remoteServerIP))
 
     try:
         for port in range(int(port_range[0]), int(port_range[1])):
+            print(port)
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             result = sock.connect_ex((remoteServerIP, port))
             if result == 0:
@@ -29,19 +30,27 @@ def scan_ports(addrP, portsP = None):
         print("cant connect to server")
         sys.exit()
 
-# main function
+
+## main function
+# checks to see if user input hostname/ip address argument
 try:
     addr = str(sys.argv[1])
-    print(addr)
+    print("Checking {}".format(addr))
+except:
+    print("You need a hostname or IP address!")
+    sys.exit()
+
+# checks to see if the user gave a specific port range
+try:
     opts, args = getopt.getopt(sys.argv[2:], "p:")
-    print(args)
 except getopt.GetoptError as err:
     print(err)
     sys.exit(2)
 
-# o == option
-# a == argument passed to the o
-# o, a = opts
-print(opts)
-a = None
+try:
+    o, a = opts[0]
+except:
+    o = None
+    a = None
+
 scan_ports(addr, a)
